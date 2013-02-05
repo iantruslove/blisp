6b107f99b0dc40b8ad75308ccba7cbe4a66f4f89
Ian Truslove <ian.truslove@nsidc.org>
2013-02-04 17:41:46 -0700

    Round trip processing for the first time.

---
+++ js/blisp.js
@@ -0,0 +1,172 @@
+/*! blisp - v0.0.1 - 2013-02-04
+* http://iantruslove.github.com/blisp/
+* Copyright (c) 2013 Ian Truslove; Licensed MIT */
+
+// UMD "returnExports" pattern
+// See http://github.com/umdjs
+(function (root, factory) {
+    if (typeof exports === 'object') {
+        // Node. Does not work with strict CommonJS, but
+        // only CommonJS-like enviroments that support module.exports,
+        // like Node.
+        module.exports = factory();
+    } else if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(factory);
+    } else {
+        // Browser globals (root is window)
+        root.returnExports = factory();
+  }
+}(this, function () {
+    var exports = {},
+        Token, BooleanToken, NumberToken, BinaryExpressionToken,
+        ExpressionStatementStartToken, ExpressionStatementEndToken, Tokenizer,
+        createTokenizer, parser, generator;
+
+  parser = {
+    isBoolean: function(value) {
+      return (value === "#t" || value === "#f");
+    },
+    isNumber: function(value) {
+      return (value.match(/[^0-9]/) === null);
+    },
+    isBinaryOperation: function(value) {
+      return (value === "+");
+    },
+    isStartParen: function(value) {
+      return (value === "(");
+    },
+    isClosingParen: function(value) {
+      return (value === ")");
+    },
+    createToken: function(value) {
+      if (parser.isBoolean(value)) {
+        return new BooleanToken(value);
+      } else if (parser.isNumber(value)) {
+        return new NumberToken(value);
+      } else if (parser.isBinaryOperation(value)) {
+        return new BinaryExpressionToken(value);
+      } else if (parser.isStartParen(value)) {
+        return new ExpressionStatementStartToken(value);
+      } else if (parser.isClosingParen(value)) {
+        return new ExpressionStatementEndToken(value);
+      }
+    }
+  };
+
+  generator = {
+    // reads a single blisp statement from the start of the code provided,
+    // and returns escodegen-compatible AST string.
+    generateStatement: function (blispCode) {
+      var expressionStatement, value, firstToken, tokenizer;
+
+      expressionStatement = {
+        type: "ExpressionStatement",
+        expression: null
+      };
+
+      tokenizer = (blispCode instanceof Tokenizer ? blispCode : new Tokenizer(blispCode));
+
+      firstToken = tokenizer.first();
+      console.log("Parsed first token " + blispCode + " as " + JSON.stringify(firstToken.parse, null, 4));
+
+      if (firstToken instanceof ExpressionStatementStartToken) {
+        tokenizer = tokenizer.rest();
+        expressionStatement.expression = tokenizer.first().parse();
+        tokenizer = tokenizer.rest();
+        expressionStatement.expression.left = tokenizer.first().parse();
+        tokenizer = tokenizer.rest();
+        expressionStatement.expression.right = tokenizer.first().parse();
+      } else {
+        expressionStatement.expression = firstToken.parse();
+      }
+
+      return expressionStatement;
+    }
+  };
+
+  BooleanToken = function (token) {
+    this.token = token;
+    return this;
+  };
+  BooleanToken.prototype.parse = function () {
+    if (this.token === "#t") { return { type: "Literal", value: true }; }
+    if (this.token === "#f") { return { type: "Literal", value: false }; }
+  };
+
+  NumberToken = function (token) {
+    this.token = token;
+    return this;
+  };
+  NumberToken.prototype.parse = function () {
+    return { type: "Literal", value: parseInt(this.token, 10) };
+  };
+
+  BinaryExpressionToken = function (token) {
+    this.token = token;
+    return this;
+  };
+  BinaryExpressionToken.prototype.parse = function () {
+    return {
+      type: "BinaryExpression",
+      operator: this.token,
+      left: null,
+      right: null
+    };
+  };
+
+  ExpressionStatementStartToken = function (token) {
+    this.token = token;
+    return this;
+  };
+  ExpressionStatementStartToken.prototype.parse = function () {
+    return {};
+  };
+
+  ExpressionStatementEndToken = function (token) {
+    this.token = token;
+    return this;
+  };
+  ExpressionStatementEndToken.prototype.parse = function () {
+    return {};
+  };
+
+  Tokenizer = function (code) {
+    var untokenizedString = code.trimLeft(),
+        matchedTerms = untokenizedString.match(/^(\(|\)|(?:[^\s()]+))(.*)$/);
+
+    this.firstToken = (matchedTerms !== null ? matchedTerms[1] : "");
+    this.restOfTokens = (matchedTerms !== null ? matchedTerms[2] : "");
+
+    return this;
+  };
+
+  Tokenizer.prototype.first = function () {
+    return parser.createToken(this.firstToken.trimRight());
+  };
+
+  Tokenizer.prototype.rest = function () {
+    if (this.restOfTokens.trimRight() === "") {
+      return null;
+    }
+    return new Tokenizer(this.restOfTokens);
+  };
+
+  createTokenizer = function(code) {
+    return new Tokenizer(code);
+  };
+
+  exports.parser = parser;
+  exports.generator = generator;
+  exports.createTokenizer = createTokenizer;
+  exports.Token = Token;
+  exports.BooleanToken = BooleanToken;
+  exports.Tokenizer = Tokenizer;
+  exports.NumberToken = NumberToken;
+  exports.BinaryExpressionToken = BinaryExpressionToken;
+  exports.ExpressionStatementStartToken = ExpressionStatementStartToken;
+  exports.ExpressionStatementEndToken = ExpressionStatementEndToken;
+
+  return exports;
+}));
+
--- js/main.js
+++ js/main.js
@@ -1,167 +1,168 @@
-var blispRepl = window.blispRepl || {};
+require(['blisp', 'escodegen'], function(blisp, escodegen) {
 
-// Model
-blispRepl.replModel = (function () {
-  var model = _.extend({}, Backbone.Events),
-      commands = [],
-      buildCommand, handleNewCommand;
+  var blispRepl = window.blispRepl || {};
 
-  buildCommand = function (commandType, commandText) {
-    return { type: commandType, value: commandText };
-  };
+  // Model
+  blispRepl.replModel = (function () {
+    var model = _.extend({}, Backbone.Events),
+        commands = [],
+        buildCommand, handleNewCommand;
 
-  handleNewCommand = function (commandType, commandText) {
-    var command = buildCommand(commandType, commandText);
-    commands.push(command);
-    model.trigger("newCommand", command);
-  };
+    buildCommand = function (commandType, commandText) {
+      return { type: commandType, value: commandText };
+    };
 
-  model.addCommand = function (commandText) {
-    handleNewCommand("command", commandText);
-  };
+    handleNewCommand = function (commandType, commandText) {
+      var command = buildCommand(commandType, commandText);
+      commands.push(command);
+      model.trigger("newCommand", command);
+    };
 
-  model.addResponse = function (responseText) {
-    handleNewCommand("response", responseText);
-  };
+    model.addCommand = function (commandText) {
+      handleNewCommand("command", commandText);
+    };
 
-  model.addComment = function (commentText) {
-    handleNewCommand("comment", commentText);
-  };
+    model.addResponse = function (responseText) {
+      handleNewCommand("response", responseText);
+    };
 
-  return model;
-}());
+    model.addComment = function (commentText) {
+      handleNewCommand("comment", commentText);
+    };
 
-// Controller
-blispRepl.replController = (function (model) {
-  var controller = {}, executeBlisp, getHelpText;
+    return model;
+  }());
 
-  executeBlisp = function (blispCode) {
-    // TODO
-    return "not yet implemented";
-  };
+  // Controller
+  blispRepl.replController = (function (model) {
+    var controller = {}, executeBlisp, getHelpText;
 
-  getHelpText = function () {
-    var helpEl = document.getElementById('helpText');
-    return (helpEl ? helpEl.innerHTML : "no help available");
-  };
+    executeBlisp = function (blispCode) {
+      return eval(escodegen.generate(blisp.generator.generateStatement(blispCode)));
+    };
 
-  controller.submitCode = function (blispCode) {
-    var response, code;
-    code = blispCode.trim();
+    getHelpText = function () {
+      var helpEl = document.getElementById('helpText');
+      return (helpEl ? helpEl.innerHTML : "no help available");
+    };
 
-    model.addCommand(code);
+    controller.submitCode = function (blispCode) {
+      var response, code;
+      code = blispCode.trim();
 
-    if (code === ":help") {
-      response = getHelpText();
-      model.addComment(response);
-    } else {
-      response = executeBlisp(code);
-      model.addResponse(response);
+      model.addCommand(code);
+
+      if (code === ":help") {
+        response = getHelpText();
+        model.addComment(response);
+      } else {
+        response = executeBlisp(code);
+        model.addResponse(response);
+      }
+
+    };
+
+    return controller;
+  }(blispRepl.replModel));
+
+  // View
+  blispRepl.replView = (function (editorSelector, controller, model) {
+    var view = {},
+        $el = $(editorSelector),
+        $repl = $el.find('.repl'),
+        getNewCode, remoteLastTypedCommand, getReadyForInput,
+        onNewCommand, showNewResponse, showNewInput, showNewComment,
+        placeCaretAtEnd, setEndOfContenteditable;
+
+    getNewCode = function() {
+      return $repl.find('.currentInput').html();
+    };
+
+    remoteLastTypedCommand = function () {
+      $repl.find('.currentInput').remove();
+    };
+
+    getReadyForInput = function () {
+      $repl.append("<li class='currentInput' autocapitalize='off' spellcheck='false' autocorrect='off' contenteditable='true'></li>");
+      var el = $repl.find('.currentInput').get(0);
+      placeCaretAtEnd(el);
+    };
+
+    onNewCommand = function (command) {
+      if (command.type === "command")  {
+        showNewInput(command.value);
+      } else if (command.type === "comment") {
+        showNewComment(command.value);
+      } else if (command.type === "response") {
+        showNewResponse(command.value);
+      }
     }
 
-  };
+    showNewResponse = function (newResponse) {
+      $repl.append("<li class='output'>" + newResponse + "</li>");
+      getReadyForInput();
+    };
 
-  return controller;
-}(blispRepl.replModel));
+    showNewInput = function (newCommand) {
+      $repl.append("<li class='oldInput'>" + newCommand + "</li>");
+    };
 
-// View
-blispRepl.replView = (function (editorSelector, controller, model) {
-  var view = {},
-      $el = $(editorSelector),
-      $repl = $el.find('.repl'),
-      getNewCode, remoteLastTypedCommand, getReadyForInput,
-      onNewCommand, showNewResponse, showNewInput, showNewComment,
-      placeCaretAtEnd, setEndOfContenteditable;
+    showNewComment = function (newComment) {
+      $repl.append("<li class='comment'>" + newComment + "</li>");
+      getReadyForInput();
+    };
 
-  getNewCode = function() {
-    return $repl.find('.currentInput').html();
-  };
-
-  remoteLastTypedCommand = function () {
-    $repl.find('.currentInput').remove();
-  };
-
-  getReadyForInput = function () {
-    $repl.append("<li class='currentInput' autocapitalize='off' spellcheck='false' autocorrect='off' contenteditable='true'></li>");
-    var el = $repl.find('.currentInput').get(0);
-    placeCaretAtEnd(el);
-  };
-
-  onNewCommand = function (command) {
-    if (command.type === "command")  {
-      showNewInput(command.value);
-    } else if (command.type === "comment") {
-      showNewComment(command.value);
-    } else if (command.type === "response") {
-      showNewResponse(command.value);
+    placeCaretAtEnd = function (el) {
+      // from http://stackoverflow.com/questions/4233265/contenteditable-set-caret-at-the-end-of-the-text-cross-browser
+      el.focus();
+      if (typeof window.getSelection != "undefined" && typeof document.createRange != "undefined") {
+        var range = document.createRange();
+        range.selectNodeContents(el);
+        range.collapse(false);
+        var sel = window.getSelection();
+        sel.removeAllRanges();
+        sel.addRange(range);
+      } else if (typeof document.body.createTextRange != "undefined") {
+        var textRange = document.body.createTextRange();
+        textRange.moveToElementText(el);
+        textRange.collapse(false);
+        textRange.select();
+      }
     }
-  }
 
-  showNewResponse = function (newResponse) {
-    $repl.append("<li class='output'>" + newResponse + "</li>");
+    setEndOfContenteditable = function (contentEditableElement) {
+      // from http://stackoverflow.com/questions/1125292/how-to-move-cursor-to-end-of-contenteditable-entity/3866442#3866442
+      var range,selection;
+      if (document.createRange) { //Firefox, Chrome, Opera, Safari, IE 9+
+        range = document.createRange();
+        range.selectNodeContents(contentEditableElement);
+        range.collapse(false);
+        selection = window.getSelection();
+        selection.removeAllRanges();
+        selection.addRange(range);
+      } else if (document.selection) { //IE 8 and lower
+        range = document.body.createTextRange();
+        range.moveToElementText(contentEditableElement);
+        range.collapse(false);
+        range.select();
+      }
+    }
+
+    $el.keydown(function (e) {
+      if (/*e.ctrlKey &&*/ e.keyCode == 13) {
+        // Ctrl-Enter pressed
+        var newCode;
+        e.preventDefault();
+        newCode = getNewCode();
+        remoteLastTypedCommand();
+        controller.submitCode(newCode);
+      }
+    });
+
+    model.on("newCommand", onNewCommand, view);
     getReadyForInput();
-  };
 
-  showNewInput = function (newCommand) {
-    $repl.append("<li class='oldInput'>" + newCommand + "</li>");
-  };
+    return view;
+  }('.blispInput', blispRepl.replController, blispRepl.replModel));
 
-  showNewComment = function (newComment) {
-    $repl.append("<li class='comment'>" + newComment + "</li>");
-    getReadyForInput();
-  };
-
-  placeCaretAtEnd = function (el) {
-    // from http://stackoverflow.com/questions/4233265/contenteditable-set-caret-at-the-end-of-the-text-cross-browser
-    el.focus();
-    if (typeof window.getSelection != "undefined" && typeof document.createRange != "undefined") {
-      var range = document.createRange();
-      range.selectNodeContents(el);
-      range.collapse(false);
-      var sel = window.getSelection();
-      sel.removeAllRanges();
-      sel.addRange(range);
-    } else if (typeof document.body.createTextRange != "undefined") {
-      var textRange = document.body.createTextRange();
-      textRange.moveToElementText(el);
-      textRange.collapse(false);
-      textRange.select();
-    }
-  }
-
-  setEndOfContenteditable = function (contentEditableElement) {
-    // from http://stackoverflow.com/questions/1125292/how-to-move-cursor-to-end-of-contenteditable-entity/3866442#3866442
-    var range,selection;
-    if (document.createRange) { //Firefox, Chrome, Opera, Safari, IE 9+
-      range = document.createRange();
-      range.selectNodeContents(contentEditableElement);
-      range.collapse(false);
-      selection = window.getSelection();
-      selection.removeAllRanges();
-      selection.addRange(range);
-    } else if (document.selection) { //IE 8 and lower
-      range = document.body.createTextRange();
-      range.moveToElementText(contentEditableElement);
-      range.collapse(false);
-      range.select();
-    }
-  }
-
-  $el.keydown(function (e) {
-    if (/*e.ctrlKey &&*/ e.keyCode == 13) {
-      // Ctrl-Enter pressed
-      var newCode;
-      e.preventDefault();
-      newCode = getNewCode();
-      remoteLastTypedCommand();
-      controller.submitCode(newCode);
-    }
-  });
-
-  model.on("newCommand", onNewCommand, view);
-  getReadyForInput();
-
-  return view;
-}('.blispInput', blispRepl.replController, blispRepl.replModel));
-
-
+});
