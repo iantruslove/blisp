e8ce66fea4f84b85caf0b47ba8496ebe5d335844
Ian Truslove <ian.truslove@nsidc.org>
2013-03-03 18:13:16 -0700

    Added scratchfile

---
+++ scratch.js
@@ -0,0 +1,10 @@
+var esprima = require('esprima');
+var escodegen = require('escodegen');
+var expression;
+
+expression = '[1]';
+expression = 'parseFloat("123.45",2,3)';
+expression = '1+2+3';
+ast = esprima.parse(expression);
+console.log( JSON.stringify(ast, null, 2));
+generatedJs = escodegen.generate(ast);
